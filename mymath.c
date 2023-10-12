7e8fc80ccc0fdb8f94a9ea46ceb80109aa4b165b
songys4429 <144648215+songys4429@users.noreply.github.com>
2023-10-12 11:41:13 +0900

    Create mymath.c

---
+++ mymath.c
@@ -0,0 +1,19 @@
+int add(int a, int b) {
+    return a + b;
+}
+
+int subtract(int a, int b) {
+    return a - b;
+}
+
+int multiply(int a, int b) {
+    return a * b;
+}
+
+int divide(int a, int b) {
+    if (b != 0) {
+        return a / b;
+    } else {
+        return -1; // 오류: 0으로 나눌 수 없음
+    }
+}
